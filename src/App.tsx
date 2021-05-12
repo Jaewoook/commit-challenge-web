26b437227ca774802a67916cc76911252ee9b740
Jaewook Ahn <ajw4586@gmail.com>
2021-05-13 02:12:53 +0900

    Update App with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Layout } from "./components";
+import * as pages from "./pages";
+import { User, WatchContext } from "./contexts/WatchContext";
+import { theme } from "./theme";
+
+const mockUsers: User[] = [{
+    email: "ajw4586@gmail.com",
+    name: "jaewoook",
+}, {
+    email: "octocat@github.com",
+    name: "octocat",
+}];
+
+const App = () => {
+    const [users, setUsers] = useState<User[]>(mockUsers);
+    const addUser = useCallback((user: User) => {
+        if (users.map((u) => u.email).includes(user.email)) {
+            return;
+        }
+        setUsers([...users, user]);
+    }, [users]);
+    const watchContext = {
+        users,
+        addUser,
+    };
+
+    return (
+        <ChakraProvider theme={theme}>
+            <WatchContext.Provider value={watchContext}>
+                <BrowserRouter>
+                    <Layout>
+                        <Switch>
+                            <Route component={pages.Home} />
+                        </Switch>
+                    </Layout>
+                </BrowserRouter>
+            </WatchContext.Provider>
+        </ChakraProvider>
+    );
+};
 
 export default App;
