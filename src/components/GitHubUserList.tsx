5c85ef5b0e7cee787358ae97aac10669f35e9aeb
Jaewook Ahn <ajw4586@gmail.com>
2021-05-13 19:20:15 +0900

    Improve GitHubUserList component by adding exist mark
    - prevent adding user if user already exist

--- src/components/GitHubUserList.tsx
+++ src/components/GitHubUserList.tsx
@@ -1,17 +1,28 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import styled from "@emotion/styled";
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Icon, Text } from "@chakra-ui/react";
 import { display, DisplayProps } from "styled-system";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 import { User, WatchContext } from "../contexts/WatchContext";
 
+const GitHubUserCard: React.FC<User> = (props) => {
+    const watch = useContext(WatchContext);
+    const { id, name, profileImageUrl } = props;
+    const exist = useMemo(() => {
+        console.log(watch.users?.map((u) => u.id));
+        return watch.users?.map((u) => u.id).includes(id);
+    }, [id, watch.users]);
 
-const GitHubUser: React.FC<User> = (props) => {
-    const { name, profileImageUrl } = props;
+    const handleClick = useCallback(() => {
+        watch.addUser(props);
+    }, [props, watch]);
+
     return (
-        <Flex minWidth="33%" alignItems="center" px="12px" py="8px" cursor="pointer" _hover={{ backgroundColor: "#eee" }}>
+        <Flex minWidth="33%" alignItems="center" px="12px" py="8px" cursor="pointer" _hover={{ backgroundColor: "#eee" }} onClick={handleClick}>
             <Image src={profileImageUrl} boxSize="35px" borderRadius="full" />
-            <Text ml="12px" fontSize="14px" fontFamily="Teko" color="#000">@{name.toUpperCase()}</Text>
+            <Text ml="12px" flex="1" fontSize="14px" fontFamily="Teko" color="#000">@{name.toUpperCase()}</Text>
+            {exist ? <Icon color="#4aa96c" as={AiFillCheckCircle} /> : null}
         </Flex>
     );
 };
@@ -35,12 +46,10 @@ interface Props {
 }
 
 export const GitHubUserList: React.FC<Props> = ({ users, show }) => {
-    const watch = useContext(WatchContext);
-
     return (
         <Wrapper display={show ? "flex" : "none"}>
-            {users.map((u) => (
-                <GitHubUser key={u.id} name={u.name} id={u.id} profileImageUrl={u.profileImageUrl} />
+            {users.map((u, i) => (
+                <GitHubUserCard key={i} name={u.name} id={u.id} profileImageUrl={u.profileImageUrl} />
             ))}
         </Wrapper>
     );
